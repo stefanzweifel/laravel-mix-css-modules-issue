42b2e40211aa50d6540ce9e97235dcf210cca0b0
Stefan Zweifel <hello@stefanzweifel.io>
2019-01-15 10:33:37 +0100

    Add oneOf Webpack Rule

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,36 @@ const mix = require('laravel-mix');
 
 mix
     .js('src/app.js', 'dist/app.js')
+
+
+
+// mix.webpackConfig({
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 oneOf: [
+//                     {
+//                         resourceQuery: /module/,
+//                         use: [
+//                             'vue-style-loader',
+//                             {
+//                                 loader: 'css-loader',
+//                                 options: {
+//                                     modules: true,
+//                                     localIdentName: '[local]_[hash:base64:5]'
+//                                 }
+//                             }
+//                         ]
+//                     },
+//                     {
+//                         use: [
+//                             'style-loader',
+//                             { loader: 'css-loader', options: { importLoaders: 1, modules: false } },
+//                         ]
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// });
