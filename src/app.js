a07aa1d3a5061d8539d87d82fe6711233716a807
Stefan Zweifel <hello@stefanzweifel.io>
2019-01-15 09:32:14 +0100

    Update to Mix 4

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 window.Vue = require('vue');
 
-Vue.component('example-component', require('./Example.vue'));
+Vue.component('example-component', require('./Example.vue').default);
 
 const app = new Vue({
     el: '#app'
